25b882bb04216575f0516491d05a5d6175677e96
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-08 17:58:14 +0400

    pm72: Update main.models

--- shop/main/models.py
+++ shop/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+
+    def __str__(self):
+        return self.name
