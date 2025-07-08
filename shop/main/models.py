9b5226928eb86f4f4a534e78423ca85c0ac45a15
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-08 18:05:22 +0400

    pm72

--- shop/main/models.py
+++ shop/main/models.py
@@ -6,5 +6,33 @@ class Category(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
 
 
+    class Meta:
+        ordering =('name',)
+        verbose_name = 'კატეგორია'
+        verbose_name_plural = 'კატეგორიები'
+
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'პროდუქტი'
+        verbose_name_plural = 'პროდუქტები'
+
+    
     def __str__(self):
         return self.name
