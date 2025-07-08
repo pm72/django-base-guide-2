f9f12b66143c7627485e0789adc15f2fd415a13b
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-08 19:15:47 +0400

    pm72

--- shop/main/models.py
+++ shop/main/models.py
@@ -2,37 +2,37 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, unique=True)
+  name = models.CharField(max_length=100, db_index=True)
+  slug = models.SlugField(max_length=100, unique=True)
 
 
-    class Meta:
-        ordering =('name',)
-        verbose_name = 'კატეგორია'
-        verbose_name_plural = 'კატეგორიები'
+  class Meta:
+    ordering =('name',)
+    verbose_name = 'კატეგორია'
+    verbose_name_plural = 'კატეგორიები'
 
 
-    def __str__(self):
-        return self.name
+  def __str__(self):
+      return self.name
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d/')
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+  category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+  name = models.CharField(max_length=100, db_index=True)
+  slug = models.SlugField(max_length=100, unique=True)
+  image = models.ImageField(upload_to='products/%Y/%m/%d/')
+  description = models.TextField(blank=True)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  available = models.BooleanField(default=True)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
 
 
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'პროდუქტი'
-        verbose_name_plural = 'პროდუქტები'
+  class Meta:
+    ordering = ('name',)
+    verbose_name = 'პროდუქტი'
+    verbose_name_plural = 'პროდუქტები'
 
-    
-    def __str__(self):
-        return self.name
+
+  def __str__(self):
+    return self.name
--- shop/main/views.py
+++ shop/main/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Category, Product
+
+
+def product_list(request, category_slug=None):
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    category = None
+
+    if category_slug:
+      category = get_object_or_404(Category, slug=category_slug)
+      products = products.filter(category=category)
+
+    context = {
+      'categories': categories,
+      'products': products,
+      'category': category,
+    }
+
+    return render(request, 'main/product_list.html', context)
